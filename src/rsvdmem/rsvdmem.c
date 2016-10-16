4901b418a2ac6c7bcab6a768fd3ffd4cccca91f3
Chaille Braille <chaille@braille.com>
2016-10-16 20:58:53 +0200

    Add further checks and comments to kmods/rsvdmem

--- src/rsvdmem/rsvdmem.c
+++ src/rsvdmem/rsvdmem.c
@@ -45,7 +45,16 @@ static int rsvdmem_release(struct inode* inode, struct file* filp)
 static int rsvdmem_mmap(struct file* filp, struct vm_area_struct* vma)
 {
    int ret;
-   unsigned long vmOffset;
+   unsigned long base_pfn;
+
+   base_pfn = (mem_base << PAGE_SHIFT);
+
+   if (base_pfn != vma->vm_pgoff)
+   {
+      print(KERN_ALERT "rsvdmem: Using mmap() with %dK page frame %ld rather than %ld.\n",
+            (PAGE_SIZE >> 10), vma->vm_pgoff, base_pfn);
+      return -EAGAIN;
+   }
 
    if (vma->vm_end - vma->vm_start != mem_size)
    {
@@ -54,14 +63,18 @@ static int rsvdmem_mmap(struct file* filp, struct vm_area_struct* vma)
       return -EAGAIN;
    }
 
-   vmOffset = mem_size + (vma->vm_pgoff << PAGE_SHIFT);
-   ret = remap_pfn_range(vma, vma->vm_start, (vmOffset >> PAGE_SHIFT),
+   // vmOffset = mem_size + (vma->vm_pgoff << PAGE_SHIFT);
+   // ret = remap_pfn_range(vma, vma->vm_start, (vmOffset >> PAGE_SHIFT),
+   //                       mem_size, PAGE_SHARED);
+   ret = remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
                          mem_size, PAGE_SHARED);
    if (ret)
    {
       printk(KERN_ALERT "rsvdmem: remap_pfn_range() failed in mmap().\n");
-      return -EAGAIN;
+      return ret;
    }
+
+   return 0;
 }
 
 static ssize_t rsvdmem_write(struct file* filp, const char __user * data, size_t size, loff_t* offset)
@@ -70,14 +83,25 @@ static ssize_t rsvdmem_write(struct file* filp, const char __user * data, size_t
    unsigned long lres;
    unsigned long raddr;
 
-   if (size != sizeof(struct rsvdmem_buffref)) return -EFAULT;
+   if (size != sizeof(struct rsvdmem_buffref))
+   {
+      printk(KERN_ALERT "rsvdmem: input size %d differs from struct size %d.\n",
+             size, sizeof(struct rsvdmem_buffref));
+      return -EFAULT;
+   }
    lres = copy_from_user(&buffref, data, size);
-   if (lres != size) return -EFAULT;
+   if (lres != size)
+   {
+      printk(KERN_ALERT "rsvdmem: copy_from_user on input returned %ld rather than %ld.\n", lres, size);
+      return -EFAULT;
+   }
    raddr = relmalloc_get(relmallocHdl, buffref.size);
    lres = copy_from_user(kmemBase+raddr, buffref.buffer, buffref.size);
    if (lres != buffref.size)
    {
       relmalloc_free(relmallocHdl, raddr);
+      printk(KERN_ALERT "rsvdmem: copy_from_user on buffer returned %ld rather than %ld.\n",
+             lres, buffref.size);
       return -EFAULT;
    }
    relmalloc_buff = raddr;
@@ -90,11 +114,25 @@ static ssize_t rsvdmem_read(struct file* filp, char __user * data, size_t size, 
    struct rsvdmem_buffref buffref;
    unsigned long lres;
 
-   if (size != sizeof(struct rsvdmem_buffref)) return -EFAIL;
+   if (size != sizeof(struct rsvdmem_buffref))
+   {
+      printk(KERN_ALERT "rsvdmem: input size %d differs from struct size %d.\n",
+             size, sizeof(struct rsvdmem_buffref));
+      return -EFAULT;
+   }
    lres = copy_from_user(&buffref, data, size);
-   if (lres != size) return -EFAULT;
+   if (lres != size)
+   {
+      printk(KERN_ALERT "rsvdmem: copy_from_user on input returned %ld rather than %ld.\n", lres, size);
+      return -EFAULT;
+   }
    lres = copy_to_user(buffref.buffer, kmemBase + relmalloc_buff, buffref.size);
-   if (lres != buffref.size) return -EFAULT;
+   if (lres != buffref.size)
+   {
+      printk(KERN_ALERT "rsvdmem: copy_to_user on buffer returned %ld rather than %ld.\n",
+             lres, buffref.size);
+      return -EFAULT;
+   }
    relmalloc_free(relmallocHdl, relmalloc_buff);
    ++(*offset);
    return lres;
@@ -114,10 +152,11 @@ static int __init rsvdmem_init(void)
 {
    printk(KERN_INFO "rsvdmem: Attempting to initialize loadable kernel device driver module.\n");
 
-   // Two system command line parameter should be set in /etc/default/grub.cfg and
-   // /boot/grub2/grub.cfg (or run grub2-mkconfig).
-   // mem=<mem_base>
-   // memmap=<mem_size>\$<mem_base>
+   // A system command line parameter should be set in /etc/default/grub.cfg and
+   // /boot/grub2/grub.cfg (or run grub2-mkconfig):
+   //   memmap=<mem_size>\$<mem_base>
+   // (Note the backslash for grub2.) Ensure that the physical memory is not used
+   // by the core system (including PCI). This might be done by looking at /proc/iomem
    if (!mem_base)
    {
       printk(KERN_ALERT "rsvdmem: Parameter mem_base not set to base of reserved memory area.\n");
@@ -130,26 +169,39 @@ static int __init rsvdmem_init(void)
    }
    printk(KERN_INFO "rsvdmem: Using mem_base=%ld mem_size=%ld\n", mem_base, mem_size);
 
+   if (mem_base & ((1 << PAGE_SHIFT) - 1))
+   {
+      printk(KERN_ALERT "rsvdmem: Parameter mem_base is not aligned to page size %dB.\n", PAGE_SIZE);
+      return -EFAULT;
+   }
+   if (mem_size & ((1 << PAGE_SHIFT) - 1))
+   {
+      printk(KERN_ALERT "rsvdmem: Parameter mem_size is not aligned to page size %dB.\n", PAGE_SIZE);
+      return -EFAULT;
+   }
+
    kmemBase = ioremap(mem_base, mem_size);
    if (!kmemBase)
    {
       printk(KERN_ALERT "rsvdmem: Unable to ioremap reserved memory.\n");
       return -EFAULT;
    }
+   printk(KERN_INFO "rsvdmem: ioremap of reserved memory completed.\n");
 
    relmallocHdl = relmalloc_init(mem_base, mem_size);
    if (!relmallocHdl)
    {
-      printk(KERN_ALERT "rsvdmem: Unable to ceate contiguous memory allocator.\n");
+      printk(KERN_ALERT "rsvdmem: Unable to create contiguous memory allocator.\n");
       return -EFAULT;
    }
 
    majorNumber = register_chrdev(0, DEVICE_NAME, fops);
    if (majorNumber < 0)
    {
-      printk(KEN_ALERT "rsvdmem: Failed to register major device number.\n");
+      printk(KERN_ALERT "rsvdmem: Failed to register major device number.\n");
       return majorNumber;
    }
+   printk(KERN_INFO "rsvdmem: Successfully allocated major device number %d.\n", majorNumber);
 
    devClass = class_create(THIS_MODULE, DEVICE_CLASS);
    if (IS_ERR(devClass))
@@ -158,6 +210,7 @@ static int __init rsvdmem_init(void)
       printk(KERN_ALERT "rsvdmem: Failed to register device class.\n");
       return PTR_ERR(devClass);
    }
+   printk(KERN_INFO "rsvdmem: Successfully registered device class.\n");
 
    rmemDev = device_create(devClass, NULL, MKDEV(majorNumber,MINOR_NUMBER), NULL, DEVICE_NAME);
    if (IS_ERR(rmemDev))
@@ -167,6 +220,7 @@ static int __init rsvdmem_init(void)
       printk(KERN_ALERT "rsvdmem: Failed to create the class.\n");
       return PTR_ERR(rmemDev);
    }
+   printk(KERN_INFO "rsvdmem: Successfully created device class.\n");
 
    printk(KERN_INFO, "rsvdmem: Initialization completed.\n");
    return 0;
